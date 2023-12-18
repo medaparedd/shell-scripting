3d6ea8e4024b49167b7ead4eb388b3912ab0eee1
medaparedd <medapareddysudhasri@gmail.com>
2023-12-18 20:10:31 +0530

    new script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -19,3 +19,14 @@ then
 else
     echo "installation success"
 fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation failed"
+    exit 1
+else
+    echo "installation success"
+fi
+
