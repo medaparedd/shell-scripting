3c1a07ccb11da587d444eda46384ffcffeecc5b0
medaparedd <medapareddysudhasri@gmail.com>
2023-12-18 21:57:11 +0530

    new script

---
+++ 11-logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+    
+    if [ $1 -ne 0 ]
+then
+    echo "$2 ... FAILED"
+    exit 1
+else
+    echo "$2 ...success"
+fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "not a root user"
+    exit 1
+else
+    echo "root user"
+fi
+
+
+
+yum install mysql -y
+
+VALIDATE $? "mysql installing"
+
+yum install git -y
+
+VALIDATE $? "git installing"
