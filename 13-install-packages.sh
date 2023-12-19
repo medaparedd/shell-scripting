1ef4f89d49330a5271ee8404151b747d8b2c00da
medaparedd <medapareddysudhasri@gmail.com>
2023-12-19 08:53:54 +0530

    this is new program

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 
 if [ $ID -ne 0 ]
 then
-    echo "not a root user"
+    echo -e "$R not a root user $N"
     exit 1
 else
-    echo "root user"
+    echo -e "$G root user $N"
 fi
 
 echo "all arguments passed: $@"
