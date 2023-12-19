19976b09ecd2edf7e0c1eb73d2caf3751bfc6726
medaparedd <medapareddysudhasri@gmail.com>
2023-12-19 09:32:44 +0530

    this is new program

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -6,12 +6,17 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script starts executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2 ...$R FAILED $N"
     else
-        echo -e "$2 ...$G SUCCESS $N"
+        echo -e "$2 ...$G SUCCESS $N"date +%F-%H-%M-%S
     fi
 }
 
@@ -28,9 +33,12 @@ fi
 
 for package in $@
 do 
-   yum list installed $package
+   yum list installed $package &>> $LOGFILE
    if [ $? -ne 0 ]
    then
-        yum install $package -y
+        yum install $package -y &>> $LOGFILE
         VALIDATE $? "installation of package"
+   else
+        echo -e "$package is already installed ... $Y SKIP $N"
+   fi
 done
