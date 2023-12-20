def2c628108646a4a280c600f14cf1ae112a14ad
medaparedd <medapareddysudhasri@gmail.com>
2023-12-20 15:19:54 +0530

    this is new one

--- greetings.sh
+++ greetings.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "hello $PERSON, good morning. we are learning shell script"
+USAGE(){
+     echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+     echo "options::"
+     echo " -n, Specify the name (mandatory)"
+     echo " -w, Specify the wishes. ex, good morning"
+     echo " -h, display help and exit"
+}
+
+while getopts ":n:w:h" opt; do
+     case $opt in 
+       n) NAME="$OPTARG";;
+       w) WISHES="$OPTARG";;
+       h|*) USAGE; exit;;
+    esac
+done
